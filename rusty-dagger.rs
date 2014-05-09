e549effbfab06dd18c0b9a9be6a6041f5f108c0e
Lindsey Bieda <lbieda@articulate.com>
2014-05-09 11:09:03 -0400

    More dots.

--- rusty-dagger.rs
+++ rusty-dagger.rs
@@ -15,6 +15,25 @@ fn main()
   /* Update the screen. */
   refresh();
 
+  let mut max_x = 0;
+  let mut max_y = 0;
+  getmaxyx(stdscr, &mut max_y, &mut max_x);
+  let mut map = vec![];
+
+  /* fill up our map and screen with dots */
+  for i in range(0, max_y) {
+    let mut map_str : ~str= "".to_owned();
+    for j in range(0, max_x) {
+      map_str = map_str + ".";
+      move(i,j);
+      printw(".");
+    }
+    map.push(map_str);
+  }
+
+  move(10,10);
+  printw("@");
+
   /* Wait for a key press. */
   getch();
 
