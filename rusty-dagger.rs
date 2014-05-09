96a76b4b24b9c57668d1768d95413ee572ab02ef
Lindsey Bieda <lbieda@articulate.com>
2014-05-09 14:42:14 -0400

    Moving structs and such into module

--- rusty-dagger.rs
+++ rusty-dagger.rs
@@ -1,8 +1,9 @@
 #![feature(globs)]
 
 extern crate ncurses;
-
 use ncurses::*;
+use world::*;
+mod world;
 
 static KEY_ESC : i32 = 27;
 
@@ -18,72 +19,6 @@ fn main()
   let mut max_x = 0;
   let mut max_y = 0;
   getmaxyx(stdscr, &mut max_y, &mut max_x);
-
-  /* TODO: move to own file */
-  struct World {
-    max_x: i32,
-    max_y: i32,
-    map: std::vec::Vec<std::strbuf::StrBuf>,
-    player: Creature
-  };
-
-  impl World {
-    fn new(max_x: i32, max_y: i32) -> World {
-      let mut map = vec![];
-      /* fill up our map and screen with dots */
-      for i in range(0, max_y) {
-        let mut map_str = StrBuf::from_str("");
-        for j in range(0, max_x) {
-          map_str.push_char('.');
-        }
-        move(i,0);
-        printw(map_str.to_str());
-        map.push(map_str);
-      }
-
-      /* create the player */
-      let mut player = Creature::new(10, 10);
-
-      World { max_x: max_x, max_y: max_y, map: map, player: player }
-    }
-
-    fn draw(&self) {
-      /* draw the map */
-      for i in range(0, self.max_y) {
-        move(i,0);
-        /* TODO: index to the correct position in the array */
-        printw(self.map.get(0).to_str());
-      }
-
-      /* move and show the player */
-      move(self.player.y, self.player.x);
-      printw("@");
-    }
-
-    fn move_player(&mut self, x: i32, y: i32) {
-      if x >= 0 && x < self.max_x && y >= 0 && y < self.max_y {
-        self.player.move(x,y);
-      } 
-    }
-  };
-
-  /* TODO: move to separate file */
-  struct Creature {
-    x: i32,
-    y: i32
-  };
-
-  impl Creature {
-    fn new(x: i32, y: i32) -> Creature {
-      Creature { x: x, y: y }
-    }
-
-    /* TODO: learn how to just do this from the vars */
-    fn move(&mut self, x: i32, y: i32) {
-      self.x = x;
-      self.y = y;
-    }
-  }
 
   /* create the world */
   let mut world = World::new(max_x, max_y);
---
+++ world.rs
@@ -0,0 +1,69 @@
+extern crate std;
+extern crate ncurses;
+
+use ncurses::*;
+
+struct Creature {
+  pub x: i32,
+  pub y: i32
+}
+
+impl Creature {
+  pub fn new(x: i32, y: i32) -> Creature {
+    Creature { x: x, y: y }
+  }
+
+  /* TODO: learn how to just do this from the vars */
+  pub fn move(&mut self, x: i32, y: i32) {
+    self.x = x;
+    self.y = y;
+  }
+}
+
+/* TODO: move to own file */
+pub struct World {
+  pub max_x: i32,
+  pub max_y: i32,
+  pub map: std::vec::Vec<std::strbuf::StrBuf>,
+  pub player: Creature
+}
+
+impl World {
+  pub fn new(max_x: i32, max_y: i32) -> World {
+    let mut map = vec![];
+    /* fill up our map and screen with dots */
+    for i in range(0, max_y) {
+      let mut map_str = StrBuf::from_str("");
+      for j in range(0, max_x) {
+        map_str.push_char('.');
+      }
+      move(i,0);
+      printw(map_str.to_str());
+      map.push(map_str);
+    }
+
+    /* create the player */
+    let mut player = Creature::new(10, 10);
+
+    World { max_x: max_x, max_y: max_y, map: map, player: player }
+  }
+
+  pub fn draw(&self) {
+    /* draw the map */
+    for i in range(0, self.max_y) {
+      move(i,0);
+      /* TODO: index to the correct position in the array */
+      printw(self.map.get(0).to_str());
+    }
+
+    /* move and show the player */
+    move(self.player.y, self.player.x);
+    printw("@");
+  }
+
+  pub fn move_player(&mut self, x: i32, y: i32) {
+    if x >= 0 && x < self.max_x && y >= 0 && y < self.max_y {
+      self.player.move(x,y);
+    } 
+  }
+}
